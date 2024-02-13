d705c68060710ba2a9ee33c8ae65476e5a2b8a33
SemenSMR <svorovali@mail.ru>
2024-02-13 15:19:44 +0300

    Init testContener

--- src/test/java/org/exapmle/autorizationserver/AutorizationServerApplicationTests.java
+++ src/test/java/org/exapmle/autorizationserver/AutorizationServerApplicationTests.java
@@ -1,13 +1,42 @@
 package org.exapmle.autorizationserver;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
-class AutorizationServerApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    @Test
-    void contextLoads() {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+    static final GenericContainer<?> myAppDev = new GenericContainer<>("devapp")
+            .withExposedPorts(8080);
+    static final GenericContainer<?> myAppProd = new GenericContainer<>("prodapp")
+            .withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        myAppDev.start();
+        myAppProd.start();
     }
 
+    @Test
+
+    void testDevApp() {
+        ResponseEntity<String> forEntityDev = restTemplate.getForEntity("http://localhost:" + myAppDev.getMappedPort(8080), String.class);
+        assertEquals("Current profile is dev", forEntityDev.getBody());
+    }
+
+
+
+    @Test
+    void testProdApp() {
+        ResponseEntity<String> forEntityProd = restTemplate.getForEntity("http://localhost:" + myAppProd.getMappedPort(8081), String.class);
+        assertEquals("Current profile is ptoduction", forEntityProd.getBody());
+    }
 }
